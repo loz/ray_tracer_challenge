354b83782582f546d8793204a90abd0ec1efb08a
Jonathan Lozinski <jonathan.lozinski@arm.com>
2020-03-20 14:58:13 +0000

    Working vector, env and render for simple 2d projectile

--- crystal/projective_img.cr
+++ crystal/projective_img.cr
@@ -1,7 +1,29 @@
 require "./src/RayTracer"
 
-c = Canvas.new(10,10)
+
+
+
+def tick(env, proj)
+  pos, vel = proj
+  grav, wind = env
+  position = pos + vel
+  velocity = vel + grav + wind
+  {position, velocity}
+end
+
+p = {point(0.0, 1.0, 0.0), vector(1.0, 1.0, 0.0).normalize }
+e = {vector(0.0, -0.1, 0.0), vector(-0.01, 0.0, 0.0)}
+
+c = Canvas.new(100,100)
 red = color(1.0, 0.0, 0.0)
-c.write_pixel(5, 5, red)
+
+16.times do 
+  pos, _ = p
+  x = (pos.x * 10.0).to_i
+  y = (pos.y * 10.0).to_i
+  print "#{x}, #{y}\n"
+  c.write_pixel(x, 100-y, red)
+  p = tick(e, p)
+end
 
 c.to_ppm_file("test.ppm")
