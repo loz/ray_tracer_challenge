bb83c32cf2f5ce6202e5fb4b99b23796292b7b58
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2020-03-23 14:06:29 +0000

    Make sphere shadow test take a size const

--- crystal/sphere_shadow.cr
+++ crystal/sphere_shadow.cr
@@ -1,19 +1,22 @@
 require "./src/RayTracer"
 
 
-c = Canvas.new(100,100)
+SIZE = 500
+
+c = Canvas.new(SIZE,SIZE)
 red = color(1.0, 0.0, 0.0)
 
 s = sphere()
 
 
-100.times do |y|
-  100.times do |x|
-  r = ray(point((x-50) * 1.0, (y-50) * 1.0, -1.0), vector(0.0, 0.0, 1.0))
-  s.transform = scaling(48.0, 48.0, 48.0)
+SIZE.times do |y|
+  print "."
+  SIZE.times do |x|
+  r = ray(point((x-(SIZE/2)) * 1.0, (y-(SIZE/2)) * 1.0, -1.0), vector(0.0, 0.0, 1.0))
+  s.transform = scaling(0.48 * SIZE, 0.48 * SIZE, 0.48 * SIZE)
   hit = s.intersect(r).hit
   unless hit.null?
-    c.write_pixel(x, 99 - y, red)
+    c.write_pixel(x, SIZE - 1 - y, red)
   end
   end
 end
