6a4cd1f2e598e190505baa51b5805c4da380a957
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2020-04-29 11:21:16 +0100

    Fixes scene layout

--- crystal/camera_scene.cr
+++ crystal/camera_scene.cr
@@ -8,7 +8,7 @@ floor.material.specular = 0.0
 
 
 left_wall = sphere()
-left_wall.transform = translation(0.0, 0.0, 0.5) *
+left_wall.transform = translation(0.0, 0.0, 5.0) *
                       rotation_y(-Math::PI/4.0) *
                       rotation_x( Math::PI/2.0) *
                       scaling(10.0, 0.01, 10.0)
@@ -16,7 +16,7 @@ left_wall.transform = translation(0.0, 0.0, 0.5) *
 left_wall.material = floor.material
 
 right_wall = sphere()
-right_wall.transform = translation(0.0, 0.0, 0.5) *
+right_wall.transform = translation(0.0, 0.0, 5.0) *
                        rotation_y( Math::PI/4.0) *
                        rotation_x( Math::PI/2.0) *
                        scaling(10.0, 0.01, 10.0)
@@ -26,7 +26,7 @@ right_wall.material = floor.material
 middle = sphere()
 middle.transform = translation(-0.5, 1.0, 0.5)
 middle.material = material()
-middle.material.color = color(0.1, 1.0, 0.5)
+middle.material.color = color(0.1, 1.0, 0.1)
 middle.material.diffuse = 0.7
 middle.material.specular = 0.3
 
@@ -34,7 +34,7 @@ right = sphere()
 right.transform = translation(1.5, 0.5, -0.5) *
                   scaling(0.5, 0.5, 0.5)
 right.material = material()
-right.material.color = color(0.5, 1.0, 0.1)
+right.material.color = color(0.1, 0.1, 1.0)
 right.material.diffuse = 0.7
 right.material.specular = 0.3
 
@@ -42,7 +42,7 @@ left = sphere()
 left.transform = translation(-1.5, 0.33, -0.75) *
                   scaling(0.33, 0.33, 0.33)
 left.material = material()
-left.material.color = color(1.0, 0.8, 0.1)
+left.material.color = color(1.0, 0.1, 0.1)
 left.material.diffuse = 0.7
 left.material.specular = 0.3
 
@@ -56,10 +56,12 @@ world.objects << left
 
 world.light = point_light(point(-10.0, 10.0, -10.0), color(1.0, 1.0, 1.0))
 
-camera = camera(250, 125, Math::PI/3.0)
+camera = camera(500, 250, Math::PI/3.0)
 camera.transform = view_transform(point(0.0, 1.5, -5.0),
                                   point(0.0, 1.0, 0.0),
                                   vector(0.0, 1.0, 0.0))
+puts "Rendering..."
 canvas = camera.render(world)
 
+puts "Saving..."
 canvas.to_ppm_file("camera_scene.ppm")
